f151083b96df368d4883f792d8c5de09328bfddc
Natalia Natsumy <natsumyiass@gmail.com>
2014-08-27 14:19:11 -0300

    Modificando a classe teste pela segunda vez

--- Documents/NetBeansProjects/teste2/src/teste2/Teste2.java
+++ Documents/NetBeansProjects/teste2/src/teste2/Teste2.java
@@ -18,7 +18,7 @@ public class Teste2 {
     public static void main(String[] args) {
         System.out.println("oi");
         System.out.println("mundo");
-        System.out.println("Modificando o código");
+        System.out.println("Modificando o código novamente");
     }
     
 }
